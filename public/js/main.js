b6472e03fb3519d86845e013289fa5cadbb3eb6f
Kieron Garvey <16197654+ki321g@users.noreply.github.com>
2023-04-23 20:35:24 +0100

    Added Error checking to Forms
    Edited addStation and addReadings forms

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,122 @@ const deleteReading = document.querySelector('#delete-reading');
 burgerIcon.addEventListener('click', () => {
 	navbarMenu.classList.toggle('is-active');
 });
+
+
+// Validates addReading Form
+function validateReadingForm() {
+	var valid = true;
+	var checkCode = document.forms["addReading"]["code"].value;
+	var checkTemperature = document.forms["addReading"]["temperature"].value;
+	var checkWindSpeed = document.forms["addReading"]["windSpeed"].value;
+	var checkWindDirection = document.forms["addReading"]["windDirection"].value;
+	var checkPressure = document.forms["addReading"]["pressure"].value;
+
+	if (checkCode == "") {
+		document.forms["addReading"]["code"].classList.add('is-danger');
+		document.getElementById("selectCode").classList.add('is-danger');
+		document.getElementById("errTemperature").style.display = "block";
+		document.getElementById("errCode").innerHTML = "Select a Code";
+		valid = false;
+	} else {
+		document.forms["addReading"]["code"].classList.add('is-success');
+		document.forms["addReading"]["code"].classList.remove('is-danger');
+		document.getElementById("selectCode").classList.add('is-success');
+		document.getElementById("selectCode").classList.remove('is-danger');
+		document.getElementById("errCode").style.display = "none";
+	}
+
+	if (checkTemperature == "") {
+		document.forms["addReading"]["temperature"].classList.add('is-danger');
+		document.getElementById("errTemperature").style.display = "block";
+		document.getElementById("errTemperature").innerHTML = "Enter the Temperature";
+		valid = false;
+	} else {
+		document.forms["addReading"]["temperature"].classList.add('is-success');
+		document.forms["addReading"]["temperature"].classList.remove('is-danger');
+		document.getElementById("errTemperature").style.display = "none";
+	}
+
+	if (checkWindSpeed == "") {
+		document.forms["addReading"]["windSpeed"].classList.add('is-danger');
+		document.getElementById("errWindSpeed").style.display = "block";
+		document.getElementById("errWindSpeed").innerHTML = "Enter the Wind Speed";
+		valid = false;
+	} else {
+		document.forms["addReading"]["windSpeed"].classList.add('is-success');
+		document.forms["addReading"]["windSpeed"].classList.remove('is-danger');
+		document.getElementById("errWindSpeed").style.display = "none";
+	}
+
+	if (checkWindDirection == "") {
+		document.forms["addReading"]["windDirection"].classList.add('is-danger');
+		document.getElementById("errWindDirection").style.display = "block";
+		document.getElementById("errWindDirection").innerHTML = "Enter the Wind Speed";
+		valid = false;
+	} else {
+		document.forms["addReading"]["windDirection"].classList.add('is-success');
+		document.forms["addReading"]["windDirection"].classList.remove('is-danger');
+		document.getElementById("errWindDirection").style.display = "none";
+	}
+
+	if (checkPressure == "") {
+		document.forms["addReading"]["pressure"].classList.add('is-danger');
+		document.getElementById("errPressure").style.display = "block";
+		document.getElementById("errPressure").innerHTML = "Enter the Pressure";
+		valid = false;
+	} else {
+		document.forms["addReading"]["pressure"].classList.add('is-success');
+		document.forms["addReading"]["pressure"].classList.remove('is-danger');
+		document.getElementById("errPressure").style.display = "none";
+	}
+
+	return valid;
+}
+
+// Validates addStationForm
+function validateStationForm() {
+	var valid = true;
+	var checkName = document.forms["addStation"]["name"].value;
+	var checkLatitude = document.forms["addStation"]["latitude"].value;
+	var checkLongitude = document.forms["addStation"]["longitude"].value;
+
+	if (checkName == "") {
+		document.forms["addStation"]["name"].classList.add('is-danger');
+		document.getElementById("errName").style.display = 'block';
+		document.getElementById("errName").innerHTML = "Enter the Station Name";
+		valid = false;
+	} else {
+		document.forms["addStation"]["name"].classList.add('is-success');
+		document.forms["addStation"]["name"].classList.remove('is-danger');
+		document.getElementById("errName").style.display = "none";
+	}
+
+	if (checkLatitude == "") {
+		document.forms["addStation"]["latitude"].classList.add('is-danger');
+		document.getElementById("errLatitude").style.display = "block";
+		document.getElementById("errLatitude").innerHTML = "Enter the Latitude";
+		valid = false;
+	} else {
+		document.forms["addStation"]["latitude"].classList.add('is-success');
+		document.forms["addStation"]["latitude"].classList.remove('is-danger');
+		document.getElementById("errLatitude").style.display = "none";
+	}
+
+	if (checkLongitude == "") {
+		document.forms["addStation"]["longitude"].classList.add('is-danger');
+		document.getElementById("errLongitude").style.display = "block";
+		document.getElementById("errLongitude").innerHTML = "Enter the Longitude";
+		valid = false;
+	} else {
+		document.forms["addStation"]["longitude"].classList.add('is-success');
+		document.forms["addStation"]["longitude"].classList.remove('is-danger');
+		document.getElementById("errLongitude").style.display = "none";
+	}
+
+	return valid;
+}
+
+
 /*
 deleteStation.addEventListener('click', () => {
 	// Functions to open and close a modal
@@ -61,4 +177,6 @@ deleteReading.addEventListener('click', () => {
 	modalDisplay.classList.toggle('is-active');
 });
 */
+
+
 
