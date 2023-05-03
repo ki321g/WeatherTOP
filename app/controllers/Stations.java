82f87e9348705944ae908b9168884a22a2701142
Kieron Garvey <16197654+ki321g@users.noreply.github.com>
2023-05-03 09:17:50 +0100

    ADDED: StationSummary

--- app/controllers/Stations.java
+++ app/controllers/Stations.java
@@ -4,12 +4,20 @@ package controllers;
     import play.mvc.*;
     import java.util.*;
     import models.*;
+    import java.util.List;
 
 public class Stations extends Controller {
   public static void index() {
     Logger.info("Rendering Stations");
     List<Station> stations = Station.sortStations(Station.findAll());
     render("stations.html", stations);
+  }
+
+  public static void summary() {
+    Logger.info("Rendering Station Summary");
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = Station.sortStations(member.stations);
+    render("stationsummary.html", stations);
   }
 }
 
